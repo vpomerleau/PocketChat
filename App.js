29e944ffa6f4e827993173be8d6deedf70d0976a
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-09-26 15:58:41 -0700

    add inital styling for nav elements

--- App.js
+++ App.js
@@ -7,17 +7,37 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./screens/Home";
 import Profile from "./screens/profile/Profile";
 import ManageItem from "./screens/ManageItem";
+import { GlobalStyles } from "./constants/styles";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function CommunicationOptions() {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="Profile" component={Profile} />
+    <BottomTabs.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary800 },
+        headerTintColor: GlobalStyles.colors.neutral1000,
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary800 },
+        tabBarActiveTintColor: GlobalStyles.colors.tertiary800,
+        tabBarInactiveTintColor: GlobalStyles.colors.neutral1000,
+      })}>
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <BottomTabs.Screen
+        name="Profile"
+        component={Profile}
+        options={{ headerShown: false }}
+      />
       {/* TODO clarify function of "edit" button in bottom nav */}
-      <BottomTabs.Screen name="Edit" component={ManageItem} />
+      <BottomTabs.Screen
+        name="Edit"
+        component={ManageItem}
+        options={{ headerShown: false }}
+      />
     </BottomTabs.Navigator>
   );
 }
@@ -27,12 +47,12 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="MainScreen"
-            component={CommunicationOptions}
-            options={{ headerShown: false }}
-          />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary800 },
+            title: "PocketChat",
+          }}>
+          <Stack.Screen name="MainScreen" component={CommunicationOptions} />
           <Stack.Screen name="ManageItem" component={ManageItem} />
         </Stack.Navigator>
       </NavigationContainer>
